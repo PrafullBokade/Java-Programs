5d95def27f6bcb7fefd4ef4408307b84ced71f5d
Prafull Bokade <102658459+PrafullBokade@users.noreply.github.com>
2022-06-03 19:32:20 +0530

    Add files via upload

---
+++ Program_100.java
@@ -0,0 +1,51 @@
+/* 
+/////////////////////////////////////////////////////////////////////
+//
+// Function name : Print 1 to 100 without usig Number
+// Output        : 1 2 3 4 5 .......100        
+// Description   : Here we can't use number to print numbers so
+//                 Hint : we can use char  
+// Author        : Prafull Bokade
+//
+//////////////////////////////////////////////////////////////////// 
+// Input :  Not a Number
+// Output : 1 2 3 4 5 .......100   
+///////////////////////////////////////////////////////////////////
+*/
+
+import java.lang.*;
+import java.util.*;
+
+
+
+class Number 
+{
+    int one = 'a'/'a';
+    int ten = "**********".length();
+
+    public int PrntNum()
+    {
+        int iCnt = one;
+
+
+        for(iCnt = one; iCnt <= (ten*ten) ; iCnt++)
+        {
+           System.out.println(iCnt);
+        }
+        return 0;
+
+    }
+
+}
+
+class Program_100
+{
+    public static void main(String b[])
+    {
+        Number nobj = new Number();
+        int iRet  =0;
+
+        nobj.PrntNum();
+        
+    }
+}
